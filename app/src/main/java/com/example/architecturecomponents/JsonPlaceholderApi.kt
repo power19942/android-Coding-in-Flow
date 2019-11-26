2e4d9d34f86247dd1eebd979f5bd191daa18b7a2
omar jakmira <poweromar19942@gmail.com>
2019-11-26 18:51:00 +0200

    clean master branch

--- app/src/main/java/com/example/architecturecomponents/JsonPlaceholderApi.kt
+++
@@ -1,9 +0,0 @@
-package com.example.architecturecomponents
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface JsonPlaceholderApi {
-    @GET("posts")
-    fun getPosts(): Call<List<Post>>
-}
