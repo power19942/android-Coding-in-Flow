a2eebb6ed31f14ef0e3b2375aeab5c93cd4b34df
omar jakmira <poweromar19942@gmail.com>
2019-12-02 21:54:12 +0200

    dynamic

--- app/src/main/java/com/example/architecturecomponents/MainActivity.kt
+++ app/src/main/java/com/example/architecturecomponents/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.example.architecturecomponents
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
-
+    var receiver = MyBroadcastReceiver()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
 
+    override fun onStart() {
+        super.onStart()
+        var filter = IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION)
+        registerReceiver(receiver,filter)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(receiver)
     }
 }
